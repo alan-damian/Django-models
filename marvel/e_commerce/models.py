ecfbcf0a6e92b54281c12a9a1dea3e2311343788
alan-damian <111703724+alan-damian@users.noreply.github.com>
2023-08-08 17:11:51 -0300

    Add files via upload

--- marvel/e_commerce/models.py
+++ marvel/e_commerce/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # NOTE: Para poder utilizar el modelo "user" que viene por defecto en Django,
 # Debemos importarlo previamente:
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -34,6 +34,38 @@ class Comic(models.Model):
         db_table = 'e_commerce_comics'
         verbose_name = 'comic'
         verbose_name_plural = 'comics'
+
+    def __str__(self):
+        '''
+        El método __str__ cumple una función parecida a __repr__ en SQL Alchemy, 
+        es lo que retorna cuando llamamos al objeto.
+        '''
+        return f'{self.id}'
+
+
+class WishList(models.Model):
+    id = models.BigAutoField(db_column='ID', primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default='')
+    
+    comic = models.ForeignKey(
+    Comic,
+    verbose_name='Comic',
+    on_delete=models.CASCADE,
+    default=1,
+    blank=True
+)
+    favorite = models.BooleanField(default=False)
+    cart = models.BooleanField(default=False)
+    wished_qty = models.PositiveIntegerField(verbose_name='wished qty', default=0)
+    bought_qty = models.PositiveIntegerField(verbose_name='bought qty', default=0)
+
+    class Meta:
+        '''
+        Con "class Meta" podemos definir atributos de nuestras entidades como el nombre de la tabla.
+        '''
+        db_table = 'wish_list'
+        verbose_name = 'WishList'
+        verbose_name_plural = 'WishLists'
 
     def __str__(self):
         '''
